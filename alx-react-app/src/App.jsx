1ba2ec377ec81ddc2f9201539f5ec7517a3a81d0
biruk mugoro <birukmugoromg@gmail.com>
2024-08-30 15:31:45 +0300

    App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -19,6 +22,9 @@ function App() {
       </div>
       <div>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
       <h1>Vite + React</h1>
       <div className="card">
