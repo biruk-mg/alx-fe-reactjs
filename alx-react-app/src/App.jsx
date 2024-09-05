e2c79ec0811388d573a3a008d3cfbdd5ef223da9
biruk mugoro <birukmugoromg@gmail.com>
2024-09-05 17:31:38 +0300

    src/App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile name="Alice" age ="25" bio= "Loves hiking and photography" />
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
     </div>
       <h1>Vite + React</h1>
       <div className="card">
