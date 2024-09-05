fa92b25a6d7d13fa829b5c47e3b2e98daae23aec
biruk mugoro <birukmugoromg@gmail.com>
2024-09-05 17:08:17 +0300

    src/components/UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'; // Import PropTypes
 const UserProfile = (props) => {
     return (
         <div>
-            <h2>{props.name}</h2>
+            <h2 style = {{color:"red"}}>{props.name}</h2>
             <p>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
         </div>
