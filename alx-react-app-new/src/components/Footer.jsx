0815156a78476bc80f53ff6b779ddb25c1fe1a72
biruk mugoro <birukmugoromg@gmail.com>
2024-09-05 18:09:57 +0300

    alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer (){
+    return (
+        <footer style={{width:'100%', height:'50px', margin:'10px', backgroundColor:'navy', color:'white', border:'3px broken violet'}}>
+            <p style={{color:'white'}}>&copy; 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent () {
+    return (
+        <main style ={{backgroundColor: 'violet', color: 'navy', fontSize: '23px',
+                    padding: '15px', borderRadius: '50px'
+         }}>
+            <p style={{fontWeight:'700', fontSize:'28px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
