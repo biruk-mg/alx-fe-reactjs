397be3c6f8849224fe06773395dd3049a6ba79aa
biruk mugoro <birukmugoromg@gmail.com>
2024-09-06 13:52:30 +0300

    alx-react-app-props/src/components/UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,17 @@
-import UserInfo from './UserInfo';
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react'; // Importing React and useContext
+import UserContext from '../UserContext';  // Importing UserContext
 
-function ProfilePage() {
-  return <UserInfo />;
+function UserProfile() {
+  const userData = useContext(UserContext); // Using useContext to get userData from UserContext
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserProfile;
