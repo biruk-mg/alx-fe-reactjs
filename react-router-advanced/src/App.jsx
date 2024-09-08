f2e248f69df9d47b7de09abd89c23b3c7d257e7a
biruk mugoro <birukmugoromg@gmail.com>
2024-09-08 17:24:48 +0300

    react-router-advanced

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,7 +2,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Profile from './components/Profile';
-import NotFound from './components/NotFound';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+import Blog from './components/Blog';
 import BlogPost from './components/BlogPost';
 import ProtectedRoute from './components/ProtectedRoute';
 
@@ -10,10 +12,23 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        <Route path="/posts/:postId" element={<BlogPost />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+        
+        {/* Protected routes */}
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
       </Routes>
     </Router>
   );
